2f39a6e1ad6d4bf3849254897219fc7e2851b9a0
dfwdf <xmfkddl1006@gmail.com>
2023-11-11 23:59:06 +0900

    Modify Registration.module.css

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 
 function Registration() {
   const [password, setPassword] = useState("");
-
+  console.log(password);
   const {
     register,
     handleSubmit,
@@ -23,8 +23,8 @@ function Registration() {
         </div>
         <h1 className={styles.header__title}>회원가입</h1>
       </header>
-      <section className={styles.registration_container}>
-        <article className={styles.registration__profile}>
+      <section className={styles.container}>
+        <article className={styles.profile_container}>
           <img src="/assets/registration/프로필사진예시.png" alt="" />
           <button type="button">
             <img
@@ -34,112 +34,107 @@ function Registration() {
           </button>
           <input type="file" />
         </article>
-        <article className={styles.registration__form}>
-          <form onSubmit={handleSubmit(submitForm)} className={styles.items}>
-            <div className={styles.item}>
-              <label htmlFor="id">아이디</label>
-              <div>
-                <input
-                  type="text"
-                  placeholder="아이디 입력"
-                  aria-invalid={
-                    isSubmitted ? (errors.id ? "true" : "false") : undefined
-                  }
-                  {...register("id", {
-                    required: "아이디는 필수 입력입니다.",
-                    minLength: {
-                      value: 8,
-                      message: "8자리 이상 아이디를 사용하세요",
-                    },
-                    maxLength: {
-                      value: 15,
-                      message: "15자리 이하 아이디를 사용하세요",
-                    },
-                  })}
-                />
-                <span className={styles.id_warning_msg}>중복 불가능</span>
-                {errors.id && (
-                  <span className={styles.error_msg}>{errors.id.message}</span>
-                )}
-              </div>
+        <form
+          onSubmit={handleSubmit(submitForm)}
+          className={styles.container__form}
+        >
+          <div className={styles.form__items}>
+            <label htmlFor="id">아이디</label>
+            <div className={styles.input_container}>
+              <input
+                type="text"
+                placeholder="아이디 입력"
+                aria-invalid={
+                  isSubmitted ? (errors.id ? "true" : "false") : undefined
+                }
+                {...register("id", {
+                  required: "아이디는 필수 입력입니다.",
+                  minLength: {
+                    value: 8,
+                    message: "8자리 이상 아이디를 사용하세요",
+                  },
+                  maxLength: {
+                    value: 15,
+                    message: "15자리 이하 아이디를 사용하세요",
+                  },
+                })}
+              />
+              <span className={styles.id_warning_msg}>*중복 불가능</span>
+              {errors.id && (
+                <span className={styles.error_msg}>{errors.id.message}</span>
+              )}
             </div>
-            <div className={styles.item}>
-              <label htmlFor="id">비밀번호</label>
-              <div>
-                <input
-                  type="text"
-                  placeholder="비밀번호 입력"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  placeholder="비밀번호 재입력"
-                  aria-invalid={
-                    isSubmitted
-                      ? errors.password
-                        ? "true"
-                        : "false"
-                      : undefined
-                  }
-                  {...register("password", {
-                    required: "비밀번호는 필수 입력입니다.",
-                    pattern: {
-                      value: new RegExp(`${password}`, "g"),
-                      message: "비밀번호가 일치하지 않습니다.",
-                    },
-                    minLength: {
-                      value: 10,
-                      message: "10자리 이상 비밀번호를 사용하세요",
-                    },
-                    maxLength: {
-                      value: 20,
-                      message: "20자리 이하 비밀번호를 사용하세요",
-                    },
-                  })}
-                />
-                {errors.password && (
-                  <span className={styles.error_msg}>
-                    {errors.password.message}
-                  </span>
-                )}
-              </div>
+          </div>
+          <div className={styles.form__items}>
+            <label htmlFor="id">비밀번호</label>
+            <div className={styles.input_container}>
+              <input
+                type="text"
+                placeholder="비밀번호 입력"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <input
+                type="text"
+                placeholder="비밀번호 재입력"
+                aria-invalid={
+                  isSubmitted ? (errors.password ? "true" : "false") : undefined
+                }
+                {...register("password", {
+                  required: "비밀번호는 필수 입력입니다.",
+                  pattern: {
+                    value: new RegExp(`${password}`, "g"),
+                    message: "비밀번호가 일치하지 않습니다.",
+                  },
+                  minLength: {
+                    value: 10,
+                    message: "10자리 이상 비밀번호를 사용하세요",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "20자리 이하 비밀번호를 사용하세요",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className={styles.error_msg}>
+                  {errors.password.message}
+                </span>
+              )}
             </div>
-            <div className={styles.item}>
-              <label htmlFor="id">닉네임</label>
-              <div>
-                <input
-                  type="text"
-                  placeholder="닉네임 입력"
-                  aria-invalid={
-                    isSubmitted ? (errors.nick ? "true" : "false") : undefined
-                  }
-                  {...register("nick", {
-                    required: "닉네임은 필수 입력입니다.",
-                    maxLength: {
-                      value: 10,
-                      message: "10자리 이하 닉네임을 사용해주세요.",
-                    },
-                  })}
-                />
-                {errors.nick && (
-                  <span className={styles.error_msg}>
-                    {errors.nick.message}
-                  </span>
-                )}
-              </div>
+          </div>
+          <div className={styles.form__items}>
+            <label htmlFor="id">닉네임</label>
+            <div className={styles.input_container}>
+              <input
+                type="text"
+                placeholder="닉네임 입력"
+                aria-invalid={
+                  isSubmitted ? (errors.nick ? "true" : "false") : undefined
+                }
+                {...register("nick", {
+                  required: "닉네임은 필수 입력입니다.",
+                  maxLength: {
+                    value: 10,
+                    message: "10자리 이하 닉네임을 사용해주세요.",
+                  },
+                })}
+              />
+              {errors.nick && (
+                <span className={styles.error_msg}>{errors.nick.message}</span>
+              )}
             </div>
+          </div>
 
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className={styles.submit_btn}
-            >
-              회원가입
-            </button>
-          </form>
-        </article>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className={styles.submit_btn}
+          >
+            회원가입
+          </button>
+        </form>
       </section>
     </div>
   );
